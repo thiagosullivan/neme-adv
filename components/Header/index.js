2f4e27dd27f377cbfe5de8fbe7eb3fcf8b460b4e
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-01-05 11:18:09 -0300

    Menu nav section

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import LogoHeader from '../../assets/neme-logo.png';
+import MenuHeader from '../../assets/menu.png';
 import { HeaderContent } from './style';
+import Link from 'next/link';
 
 function Header() {
+
+  const [ openMenu, setOpenMenu ] = useState(false);
+
+  console.log(openMenu)
+
   return (
     <HeaderContent>
-        <nav>
-            Header
+      <div className='header__content'>
+        <Link href="/">
+          <Image src={LogoHeader} alt="Logo Neme" />
+        </Link>
+        <div className='hamb__header' onClick={() => setOpenMenu(!openMenu)}>
+          <Image src={MenuHeader} alt="Menu" />
+          <p>Menu</p>
+        </div>
+        <nav className={` ${ openMenu ? 'MenuOpen' : ''}`}>
+          <ul>
+            <li><Link href="/">Sobre</Link></li>
+            <li><Link href="/">Time</Link></li>
+            <li><Link href="/">Especialidades</Link></li>
+            <li><Link href="/">Soluções</Link></li>
+            <li><Link href="/">Contato</Link></li>
+          </ul>
         </nav>
+      </div>
     </HeaderContent>
   )
 }
--- components/Header/style.js
+++ components/Header/style.js
@@ -1,11 +1,75 @@
 import styled from "styled-components";
 
 export const HeaderContent = styled.header`
-    background-color: #0a3d5b;
-    max-width: 260px;
-    color: #ffffff;
 
-    &:after {
-        content: '';
+    .header__content {
+        background-color: #0a3d5b;
+        max-width: 260px;
+        width: 100%;
+        /* height: 360px; */
+        padding: 3.1rem 0;
+        color: #ffffff;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        z-index: 2;
+        
+        .hamb__header {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+            top: 100%;
+            left: 15%;
+            z-index: 2;
+            cursor: pointer;
+    
+            p {
+                font-family: 'Raleway', sans-serif;
+                text-transform: uppercase;
+                font-size: 24px;
+                margin-left: 1.25rem;
+            }
+        }
+
+        .MenuOpen {
+            top: 105% !important;
+            height: 530px;
+            pointer-events: all;
+        }
+        nav {
+            position: absolute;
+            top: 45%;
+            /* top: 105%; */
+            background-color: #0a3d5b;
+            width: 100%;
+            height: 0;
+            padding: 8.5rem 1.87rem 1.25rem;
+            z-index: -10;
+            transition: all 300ms ease-in;
+            overflow-y: hidden;
+            pointer-events: none;
+
+            ul {
+                li {
+                    font-family: 'Raleway', sans-serif;
+                    text-transform: uppercase;
+                    font-size: 22px;
+                    margin-bottom: 1.3rem;
+                }
+            }
+        }
+
+        &:after {
+            content: '';
+            background-image: url('https://raw.githubusercontent.com/thiagosullivan/neme-adv/main/assets/menu-after.png');
+            width: 100%;
+            height: 109px;
+            display: block;
+            position: absolute;
+            top: 100%;
+        }
     }
 `;
