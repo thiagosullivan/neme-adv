ffe26600ccd1b67004da69c0bfe8035c594a4840
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-01-05 11:18:14 -0300

    Adding assets

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,30 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet"></link>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          {/* Primary Meta Tags */}
+          <meta name="description" content="Hevelen Jeronymo, Profissional de Educação Física especialista em gerar resultados. Flexibilidade para realizar seu treino em qualquer academia, sem ficar preso a horários. Personal Trainer em Londrina-PR" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.heveleneducadorafisica.com.br/" />
+          <meta property="og:description" content="Hevelen Jeronymo, Profissional de Educação Física especialista em gerar resultados. Flexibilidade para realizar seu treino em qualquer academia, sem ficar preso a horários. Personal Trainer em Londrina-PR" />
+          <meta property="og:image" content="https://i.ibb.co/tpMhLY4/heve-metatag.jpg" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://www.heveleneducadorafisica.com.br/" />
+          <meta property="twitter:description" content="Hevelen Jeronymo, Profissional de Educação Física especialista em gerar resultados. Flexibilidade para realizar seu treino em qualquer academia, sem ficar preso a horários. Personal Trainer em Londrina-PR" />
+          <meta property="twitter:image" content="https://i.ibb.co/tpMhLY4/heve-metatag.jpg"></meta>
+        </Head>
       <body>
         <Main />
         <NextScript />
