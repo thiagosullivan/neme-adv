e355b81ab718238fb7bc501a49e55404e2b45c81
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-01-06 06:13:14 -0300

    Specialty section

---
+++ components/Especialidades/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { EspecialidadesContainer } from './styles';
+
+function Especialidades() {
+  return (
+    <EspecialidadesContainer>
+        <h2>Quem nos conhecer <br/><strong>melhor?</strong></h2>
+    </EspecialidadesContainer>
+  )
+}
+
+export default Especialidades
