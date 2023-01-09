fb5b4e43b0f9cf6b3e6205d7ecf7c7b13ad58263
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-01-09 11:42:08 -0300

    Speciality section

--- components/Especialidades/index.js
+++ components/Especialidades/index.js
@@ -1,10 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import MarkOne from '../../assets/mark-1.png';
 import { EspecialidadesContainer } from './styles';
 
 function Especialidades() {
   return (
     <EspecialidadesContainer>
-        <h2>Quem nos conhecer <br/><strong>melhor?</strong></h2>
+        <div className='speciality__contact'>
+            <div className='speciality__contact_txt'>
+                <h2>Quer nos conhecer <br/><strong>melhor?</strong></h2>
+                <Link href="/">Entre em contato!</Link>
+            </div>
+        </div>
+        <div className='speciality__content'>
+            <h2>Áreas que <br/><strong>atuamos!</strong></h2>
+            <ul>
+                <li>
+                    <Image src={MarkOne} width={52} height={52} alt="marcação" />
+                    <p>Tributário</p>
+                </li>
+                <li>
+                    <Image src={MarkOne} width={52} height={52} alt="marcação" />
+                    <p>Empresarial</p>
+                </li>
+                <li>
+                    <Image src={MarkOne} width={52} height={52} alt="marcação" />
+                    <p>Fazendo Pública</p>
+                </li>
+                <li>
+                    <Image src={MarkOne} width={52} height={52} alt="marcação" />
+                    <p>Civil</p>
+                </li>
+                <li>
+                    <Image src={MarkOne} width={52} height={52} alt="marcação" />
+                    <p>LGPD*</p>
+                </li>
+            </ul>
+        </div>
     </EspecialidadesContainer>
   )
 }
--- components/Especialidades/styles.js
+++ components/Especialidades/styles.js
@@ -2,4 +2,82 @@ import styled from "styled-components";
 
 export const EspecialidadesContainer = styled.section`
 
+    .speciality__contact {
+        padding-top: 3.75rem;
+        background-image: url('https://raw.githubusercontent.com/thiagosullivan/neme-adv/main/assets/especialidade-bg.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 500px;
+
+        .speciality__contact_txt {
+            max-width: 1100px;
+            width: 100%;
+            margin: 0 auto;
+
+            h2 {
+                font-family: 'Crimson Pro', serif;
+                font-size: 3.31rem;
+                color: ${({theme}) => theme.primary};
+                text-transform: uppercase;
+                margin-bottom: 3.37rem;
+                font-weight: 400;
+    
+                strong {
+                    font-family: 'Crimson Pro', serif;
+                }
+            }
+    
+            a {
+                font-family: 'Raleway', sans-serif;
+                font-size: 1.78rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 280px;
+                height: 62px;
+                color: ${({theme}) => theme.secondary};
+                border: 1px solid ${({theme}) => theme.secondary};
+                border-radius: 20px;
+            }
+        }
+    }
+
+    .speciality__content {
+        max-width: 1100px;
+        width: 100%;
+        margin: 0 auto 9.3rem;
+        display: flex;
+        align-items: center;
+
+        h2 {
+            font-family: 'Crimson Pro', serif;
+            font-size: 4.5rem;
+            color: ${({theme}) => theme.primary};
+            text-transform: uppercase;
+            margin-bottom: 3.37rem;
+            margin-right: 9.3rem;
+            font-weight: 400;
+
+            strong {
+                font-family: 'Crimson Pro', serif;
+            }
+        }
+        ul {
+            li {
+                display: flex;
+                align-items: center;
+                margin-bottom: 3.1rem;
+
+                &:last-child {
+                    margin-bottom: 0;
+                }
+
+                p {
+                    font-family: 'Raleway', sans-serif;
+                    font-size: 2.68rem;
+                    margin-left: 2.1rem;
+                }
+            }
+        }
+    }
 `;
